a4b2f9eb4280f0cc09f56b4723b43f2b4f3aca87
chenzhiyong <qq113717599@qq.com>
2020-05-26 19:04:34 +0800

    知识库

--- src/views/HomeOther.vue
+++ src/views/HomeOther.vue
@@ -255,6 +255,7 @@
 				})
 			},
 			guanzhu() {
+        alert(JSON.stringify(this.$route.query))
 				if(this.guanzhuContent=="关注TA"){
 					let param = {
 						"robot_id": 33,
@@ -262,7 +263,8 @@
 						"operation_type":"0",
 						"broker_id":33,
 						"token":"ZXlKMGVYQWlPaUpLVjFBaUxDSmhiR2NpT2lKa1pXWmhkV3gwSW4wOjFqVzlDcDpsal9zdVlrR0V6T3lMY1dSTnFkcXdWc2Z3V00.ZXlKUVNFOU9SU0k2SWpFM05qRXdNREkzT0Rjeklpd2lTVVFpT2pNekxDSnBZWFFpT2pFMU9EZzNNams0TXprdU1UWTVPRFF4TTMwOjFqVzlDcDptdDVjeWExajBWSG9XMzlOMVN2WGhVQ1otQzQ.0ee1173f3a6a0489b64ec92e22c60cd1"
-					}
+          }
+          console.log(param)
 					let result = guanZhu(param)
 					result.then(res => {
 						console.log("guanzhu:" + res)
