d48b88fa1e92ba8a1a661710c9c580f330ab646a
chenzhiyong <qq113717599@qq.com>
2020-05-28 10:55:03 +0800

    1.43

--- src/views/WhoLookMe.vue
+++ src/views/WhoLookMe.vue
@@ -61,11 +61,11 @@ export default {
   },
   mounted(){
     let param = {
-      "broker_id":33,
-      "robot_id":33,
-      "token":"ZXlKMGVYQWlPaUpLVjFBaUxDSmhiR2NpT2lKa1pXWmhkV3gwSW4wOjFqVzlDcDpsal9zdVlrR0V6T3lMY1dSTnFkcXdWc2Z3V00.ZXlKUVNFOU9SU0k2SWpFM05qRXdNREkzT0Rjeklpd2lTVVFpT2pNekxDSnBZWFFpT2pFMU9EZzNNams0TXprdU1UWTVPRFF4TTMwOjFqVzlDcDptdDVjeWExajBWSG9XMzlOMVN2WGhVQ1otQzQ.0ee1173f3a6a0489b64ec92e22c60cd1"
+       "broker_id": this.$route.query.broker_id,
+      "robot_id": this.$route.query.robotId,
+      "token":this.$route.query.token
     }
-    console.log(param)
+    alert(param)
     let result = reqLookMe(param)
     result.then(res=>{
       this.list = res.result
