0de3bb960ded7e34c2b6529f42fb59dbc668f156
chenzhiyong <qq113717599@qq.com>
2020-05-28 13:18:16 +0800

    4.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -443,7 +443,7 @@ export default{
               title: 'bebot朋友', // 分享标题
               desc: '描述yiuy朋友', // 分享描述
               // link: window.location.href,
-              link: 'https://test-bebot-web.baoxianxia.com.cn/#/'+'HomeOther?broker_id=that.$route.query.broker_id&robot_id=that.$route.query.robot_id&token=that.$route.query.token', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+              link: 'https://test-bebot-web.baoxianxia.com.cn/#/'+`HomeOther?broker_id=${that.$route.query.broker_id}&robot_id=${that.$route.query.robot_id}`, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
               imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590588171242&di=ac9d15a3d7da1c6e5a8c722c94c914bf&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F35%2F34%2F19300001295750130986345801104.jpg', // 分享图标
               // type: '', // 分享类型,music、video或link，不填默认为link
               // dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
