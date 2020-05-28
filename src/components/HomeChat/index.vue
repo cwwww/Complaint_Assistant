14315792b64166f026b7440866f2b0faed0c021f
chenzhiyong <qq113717599@qq.com>
2020-05-28 19:33:33 +0800

    4.1

--- src/components/HomeChat/index.vue
+++ src/components/HomeChat/index.vue
@@ -99,9 +99,8 @@ export default {
     },
     getChatList() {
       let param = {
-        broker_id: 33,
-        token:
-          "ZXlKMGVYQWlPaUpLVjFBaUxDSmhiR2NpT2lKa1pXWmhkV3gwSW4wOjFqVzlDcDpsal9zdVlrR0V6T3lMY1dSTnFkcXdWc2Z3V00.ZXlKUVNFOU9SU0k2SWpFM05qRXdNREkzT0Rjeklpd2lTVVFpT2pNekxDSnBZWFFpT2pFMU9EZzNNams0TXprdU1UWTVPRFF4TTMwOjFqVzlDcDptdDVjeWExajBWSG9XMzlOMVN2WGhVQ1otQzQ.0ee1173f3a6a0489b64ec92e22c60cd1"
+        broker_id: this.broker_id,
+        token: this.token
       };
       console.log(param);
       let res = reqRobotHistory(param);
