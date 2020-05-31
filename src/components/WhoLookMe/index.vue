fefe149cc44d7f1aabaf32d12fec9618f9b70512
chenzhiyong <qq113717599@qq.com>
2020-05-31 20:25:09 +0800

    531.1

--- src/components/WhoLookMe/index.vue
+++ src/components/WhoLookMe/index.vue
@@ -91,9 +91,9 @@ export default {
   },
   mounted() {
     let param = {
-      broker_id: 33 || this.broker_id,
-      robot_id:33 ||  this.robot_id,
-      token:"ZXlKMGVYQWlPaUpLVjFBaUxDSmhiR2NpT2lKa1pXWmhkV3gwSW4wOjFqVzlDcDpsal9zdVlrR0V6T3lMY1dSTnFkcXdWc2Z3V00.ZXlKUVNFOU9SU0k2SWpFM05qRXdNREkzT0Rjeklpd2lTVVFpT2pNekxDSnBZWFFpT2pFMU9EZzNNams0TXprdU1UWTVPRFF4TTMwOjFqVzlDcDptdDVjeWExajBWSG9XMzlOMVN2WGhVQ1otQzQ.0ee1173f3a6a0489b64ec92e22c60cd1" || this.token
+      broker_id:  this.broker_id_prop,
+      robot_id: this.robot_id_prop,
+      token: this.token_prop
     };
     let result = reqLookMe(param);
     result
