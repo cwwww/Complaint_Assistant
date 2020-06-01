c4c095796d464c60646837dcc7ebce032f0766d9
chenzhiyong <qq113717599@qq.com>
2020-06-01 09:51:26 +0800

    5310

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -160,11 +160,11 @@ export default {
           COUNTRY: that.mes.country,
           PRIVILEGE: that.mes.privilege
         };
-        console.log(param);
+        alert(param);
         let res = reqlogin(param);
         res
           .then(res => {
-            console.log(res);
+            alert(res);
             that.messages = res.result;
             // if (this.$route.query.type == "otherLogin") {
             // this.$router.push({
@@ -200,26 +200,6 @@ export default {
         .then(res => {
           this.mes = res.result;
           alert(this.mes);
-          let param = { openid: this.mes.openid };
-
-          let result = reqisregistered(param);
-          result
-            .then(result => {
-              this.loginMeg = result.result;
-              if (this.loginMeg.visitor_type == 1) {
-                this.$router.push({
-                  path: "/",
-                  query: {
-                    visitor_id: this.loginMeg.visitor_id,
-                    robot_id: this.loginMeg.robot_id,
-                    token: this.loginMeg.token
-                  }
-                });
-              }
-            })
-            .catch(reslove => {
-              console.log("error");
-            });
         })
         .catch(reslove => {
           console.log("error");
@@ -272,6 +252,25 @@ export default {
     this.code = this.url.substring(start + 1, end);
     console.log(this.url);
     this.impower();
+    let param = { openid: this.mes.openid };
+    let result = reqisregistered(param);
+    result
+      .then(result => {
+        this.loginMeg = result.result;
+        if (this.loginMeg.visitor_type == 1) {
+          this.$router.push({
+            path: "/",
+            query: {
+              visitor_id: this.loginMeg.visitor_id,
+              robot_id: this.loginMeg.robot_id,
+              token: this.loginMeg.token
+            }
+          });
+        }
+      })
+      .catch(reslove => {
+        console.log("error");
+      });
   }
 };
 </script>
