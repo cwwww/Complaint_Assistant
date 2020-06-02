54edebc43abb09b97563e0274bddb410bc8f2d7e
chenzhiyong <qq113717599@qq.com>
2020-06-02 15:51:44 +0800

    623

--- src/components/WhoLookMe/ACChat/index.vue
+++ src/components/WhoLookMe/ACChat/index.vue
@@ -118,6 +118,10 @@ export default {
   ],
   created() {
     this.chat = this.ACChat_show;
+    this.titleName_prop = this.titleName;
+    this.customer_type_prop = this.customer_type;
+    this.customerImg_prop = this.customerImg;
+    this.customer_id_prop = this.customer_id;
   },
   watch: {
     ACChat_show(newValue) {
@@ -199,10 +203,7 @@ export default {
     this.scrollToBottom();
   },
   mounted() {
-    this.titleName_prop = this.titleName;
-    this.customer_type_prop = this.customer_type;
-    this.customerImg_prop = this.customerImg;
-    this.customer_id_prop = this.customer_id;
+
     this.scrollToBottom();
     this.getDialogAgent();
     // window.setInterval(() => {
